9ede1bd4ec796813b8248adb215765b7f6e65f41
Janiel Venice Abad <janielveniceabad@gmail.com>
2020-07-10 16:58:04 -0700

    Upload controllers

---
+++ controllers/userController.js
@@ -0,0 +1,106 @@
+const multer = require('multer'); // for image uploads
+
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+
+    // user-userid-timestamp.jpg
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload images only.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id; // gets current user id to use for getOne factory fcn
+  next();
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updatePassword.'
+      )
+    );
+  }
+
+  // 2. filter out unwanted fields not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  if (req.file) {
+    filteredBody.photo = req.file.filename;
+  }
+
+  // 3. update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined. Use /signup instead.',
+  });
+};
+
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+
+// admin only; do not use to update passwords
+exports.updateUser = factory.updateOne(User);
+
+// admin delete user permanently
+exports.deleteUser = factory.deleteOne(User);
